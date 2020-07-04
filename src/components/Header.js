020ded1a416901c7076961e93125f01fd65c09c9
Aqveduktis <sara.lovens.lovn@gmail.com>
2020-07-04 19:49:58 +0200

    changing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,16 @@ import styled from 'styled-components'
 
 const Title = styled(Link)`
   font-size: 2.5em;
-  color: #3d82ab;
+  color: #706fab;
   width: 100%;
   text-align: center;
   cursor: pointer;
   &:hover {
     text-decoration: none;
+    color: #706fab;
   }
 `
-
+//  color: #3d82ab;
 
 export const Header = () => {
   return(
